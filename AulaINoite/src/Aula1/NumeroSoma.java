943c051f4a7ef286e244867922cdc9c2f296e079
Cristina Jung <cristijung@gmail.com>
2021-03-01 22:16:24 -0300

    Add files via upload

---
+++ AulaINoite/src/Aula1/NumeroSoma.java
@@ -0,0 +1,26 @@
+/*
+Criar um algoritmo que executa a soma de 2 valores
+ */
+package Aula1;
+
+public class NumeroSoma {
+    //criação do método principal
+    public static void main (String args[]) {
+        //declaração das variáveis
+        int x, y;
+        String nome;
+        
+        //atribuição dos valores das variáveis
+        x = 2;
+        y = (int) 3.0;
+        nome = "Maria";
+        
+        //escrever a saída utilizando o método System.out.println
+        //o sinal de "+" serve para concatenar as informações das variáveis
+        System.out.println("X + Y = " + (x + y) + ". O nome é? " + nome);
+        
+        
+    }
+    
+    
+}
